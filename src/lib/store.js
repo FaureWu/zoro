3bd8d4a89b5035f0f559218b281888bcded3f6da
yaochen <yaochen@guoxiaomei.com>
2018-06-20 14:48:20 +0800

    feat: remove dist

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,6 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
 
 export default ({ rootReducer, middlewares, initialState }) => {
+  // eslint-disable-next-line
+  // const composeEnhancers =
+  //   window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  //     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  //     : compose
   const store = createStore(
     rootReducer,
     initialState,
